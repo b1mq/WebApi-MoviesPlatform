ba7d734909511cb6d8306399da90a9bf649baac4
Egor <arnoldtagirov1@gmail.com>
2026-09-08 20:23:38 +0200

    cors,reactapp

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -44,7 +44,7 @@ namespace WebApplication1
                 app.UseHttpsRedirection();
                 app.UseAuthorization();
                 app.UseCors("AllowReactApp");
-                app.MapControllers();
+                app.MapControllers(); / 1
 
                 app.Run();
             }
