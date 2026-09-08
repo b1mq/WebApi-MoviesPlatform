b4f9c00d57d0ec63cd71f3bfc308af7c5cb281b4
Egor <arnoldtagirov1@gmail.com>
2026-09-08 18:38:35 +0200

    Added Application DependencyInjection

---
+++ Application/Extensions/DependencyInversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Domain.Entities;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions
+{
+    public static class ApplicationServiceExtension
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection sc)
+        {
+            sc.AddScoped<IFilmService, IFilmService>();
+            sc.AddValidatorsFromAssembly(typeof(ApplicationServiceExtension).Assembly);
+            return sc;
+        }
+    }
+}
