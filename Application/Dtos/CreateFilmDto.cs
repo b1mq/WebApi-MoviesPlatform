f56b34fa79cafa4fab3f0a3010af14175656525f
Egor <arnoldtagirov1@gmail.com>
2026-09-08 18:24:49 +0200

    Added Application Dto,Dto Validator

---
+++ Application/Dtos/CreateFilmDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class CreateFilmDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string Author { get; set; } = string.Empty;
+    }
+}
---
+++ Application/Validation/FilmValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Dtos;
+using FluentValidation;
+
+namespace Application.Validation
+{
+    public class FilmValidator:AbstractValidator<CreateFilmDto>
+    {
+        public FilmValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Title can not be empty").MaximumLength(100).WithMessage("Title can not be more than 100");
+
+        }
+    }
+}
