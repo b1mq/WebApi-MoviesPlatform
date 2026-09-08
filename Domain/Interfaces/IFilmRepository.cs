26c5c7f9c43bdbff68dac418679629efe66f9e22
Egor <arnoldtagirov1@gmail.com>
2026-09-08 15:42:55 +0200

    Created IFilmRepository

---
+++ Domain/Interfaces/IFilmRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+namespace Domain.Interfaces
+{
+    public  interface IFilmRepository
+    {
+        Task<Film?> GetFilmByTitleAsync(string title);
+        Task<Film?> GetFilmByIdAsync(int id);
+        Task<bool> DeleteFilmByIdAsync(int id);
+        Task<bool> AddNewFilmAsync(Film film);
+        Task<bool> UpdateFilmAsync(Film film);
+        Task<IEnumerable<Film>> GetAllFilmsAsync();
+
+    }
+}
